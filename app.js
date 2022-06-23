c485bd63bc9ae600f3dc896c7f318ca9a21b37ad
sid-al <sendplusx@gmail.com>
2022-06-23 13:50:17 +0300

    add eventlistner to search button and passed argurments a call-back function

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // DOM getting HTML elements
 
 const searchBtn = document.querySelector('#search-btn');
+const mealList = document.getElementById('meal')
+const mealDetailsContent = document.querySelector("meal-details-content")
+const recipeCloseBtn = document.getElementById('recipe-close-btn')
+
+// event listners
+searchBtn.addEventListener('click', renderMeal)
 
