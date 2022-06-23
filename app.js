7d0ba60714b2823739edb1e3c4518a64c97d7582
sid-al <sendplusx@gmail.com>
2022-06-24 01:03:58 +0300

    finalized

--- app.js
+++ app.js
@@ -1,55 +1,83 @@
 // DOM getting HTML elements
 
 document.addEventListener('DOMContentLoaded', () => {
-  getMealList();
+  getbooksList();
 });
 
 const searchBtn = document.getElementById('search-btn');
-const mealList = document.getElementById('meal')
-const mealDetailsContent = document.querySelector("meal-details-content")
-const recipeCloseBtn = document.getElementById('recipe-close-btn')
+const booksList = document.getElementById('books')
+const booksDetailsContent = document.querySelector("books-details-content")
+const readCloseBtn = document.getElementById('read-close-btn')
 
 // event listners
 
-searchBtn.addEventListener('click', getMealList)
-mealList.addEventListener('click', getMealRecipe);
-// getMealList that matches the search input
+searchBtn.addEventListener('click', getbooksList)
+booksList.addEventListener('click', getbooksread);
+// getbooksList that matches the search input
 
-function getMealList(){
+function getbooksList(){
   let searchInputText = document.getElementById('search-input').value.trim();
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputText}`)
+  fetch(`https://api.itbook.store/1.0/search/=${searchInputText}.json`)
   .then(resp => resp.json())
   .then (data => {
     if(data)
     console.log(data);
     let html = "";
-    if (data.meals) {
-      data.meals.forEach(meal => {
+    if (data.books) {
+      data.books.forEach(book => {
         html+= `
-          <div class = "meal-item" data-id = "${meal.idMeal}">
-              <div class = "meal-img">
-                  <img src = "${meal.strMealThumb}" alt = "food">
+          <div class = "books-item" data-id = "${book.isbn13}">
+              <div class = "books-img">
+                  <img src = "${book.image}" alt = "food">
               </div>
-              <div class = "meal-name">
-                  <h3>${meal.strMeal}</h3>
-                  <a href = "#" class = "recipe-btn">Get Recipe</a>
+              <div class = "books-name">
+                  <h3>${book.title}</h3>
+                  <a href = "#" class = "read-btn">Read more</a>
               </div>
           </div>
         `;
       }); 
-      mealList.classList.remove('notFound');
+      booksList.classList.remove('notFound');
     }else{
-      html = "Opps, could not find any meal recipe!";
-      mealList.classList.add('notFound');
+      html = "Opps, could not find any books read!";
+      booksList.classList.add('notFound');
     }
-    mealList.innerHTML = html;
+    booksList.innerHTML = html;
   });
 }
 
-function getMealRecipe(e) {
+function getbooksread(e) {
   e.preventDefault();
-  if(e.target.classList.contains('recipe-btn')){
-    let mealItem = e.target.parentElement.parentElement;
-    console.log(mealItem);
+  if(e.target.classList.contains('read-btn')){
+    let booksItem = e.target.parentElement.parentElement;
+    console.log(booksItem);
+    fetch(`https://api.itbook.store/1.0/search/=${booksItem.isbn13}.json`)
+    .then(resp => resp.json())
+    .then(data => bookReadModal(data))
   }
+}
+
+// create a modal
+function bookReadModal(book) {
+  console.log(book)
+  book = book[0];
+  let html = `
+  
+  <div class = "books-details-content">
+        <h2 class="read-title">${book.title}</h2>
+        <p class="read-category"></p>
+        <h3>description</h3>
+        <p> ${book.subtitle}.</p>
+    </div>
+    <div class = 'read-books-img'>
+      <img src="${book.image}" alt="">
+    </div>
+    <div class="read-link">
+      <a href="${book.url}" target="_blank">Get Book</a>
+  </div>
+
+  `;
+  console.log(booksDetailsContent.innerHTML = html); 
+  booksDetailsContent.parentElement.classList.add('showRead')
+  
 }
