c5da00f4b7eb93a2a52a1d4ddc5a682b516e8f33
sid-al <sendplusx@gmail.com>
2022-06-24 14:31:22 +0300

    add modified readme file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project Name :        Techy Book Search App
+
+## Problem statement.
+Finding the right materials and books to learn and practice new advanced rapidly changing  technology has become challenging.
+
+## Solution statement
+ “ I dont want not to be left behind on AI and Machine Learning” but find the right books to learn from is hard, if this is your situation then this application is meant for you just search the name of the book or technological resource you want and let us do for the donkey work of finding the right books you want.
+
+API Resource:  https://api.itbook.store/1.0/search/.json
+
+Read More on my Blog: https://sidknowal.blogspot.com
+
--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const readCloseBtn = document.getElementById('read-close-btn')
 // event listners
 
 searchBtn.addEventListener('click', getbooksList)
-booksList.addEventListener('click', getbooksread);
 // getbooksList that matches the search input
 
 function getbooksList(){
@@ -44,40 +43,4 @@ function getbooksList(){
     }
     booksList.innerHTML = html;
   });
-}
-
-function getbooksread(e) {
-  e.preventDefault();
-  if(e.target.classList.contains('read-btn')){
-    let booksItem = e.target.parentElement.parentElement;
-    console.log(booksItem);
-    fetch(`https://api.itbook.store/1.0/search/=${booksItem.isbn13}.json`)
-    .then(resp => resp.json())
-    .then(data => bookReadModal(data))
-  }
-}
-
-// create a modal
-function bookReadModal(book) {
-  console.log(book)
-  book = book[0];
-  let html = `
-  
-  <div class = "books-details-content">
-        <h2 class="read-title">${book.title}</h2>
-        <p class="read-category"></p>
-        <h3>description</h3>
-        <p> ${book.subtitle}.</p>
-    </div>
-    <div class = 'read-books-img'>
-      <img src="${book.image}" alt="">
-    </div>
-    <div class="read-link">
-      <a href="${book.url}" target="_blank">Get Book</a>
-  </div>
-
-  `;
-  console.log(booksDetailsContent.innerHTML = html); 
-  booksDetailsContent.parentElement.classList.add('showRead')
-  
 }
